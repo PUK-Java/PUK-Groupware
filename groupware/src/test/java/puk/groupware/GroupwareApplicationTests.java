a8f2c6dd01d1446acfd51980ec43d63644f0b180
Koeyh <gjcor96@gmail.com>
2024-05-27 19:05:04 +0900

    f

---
+++ groupware/src/test/java/puk/groupware/GroupwareApplicationTests.java
@@ -0,0 +1,13 @@
+package puk.groupware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
