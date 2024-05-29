a791606ee413c6c6e32f89181aa7e41c646c6c5c
vinca0224 <vinca0224@naver.com>
2024-05-29 13:54:05 +0900

    login

--- groupware/src/main/java/puk/groupware/repository/user/User_info_jpaRepository.java
+++ groupware/src/main/java/puk/groupware/repository/user/User_info_jpaRepository.java
@@ -3,27 +3,12 @@ package puk.groupware.repository.user;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-<<<<<<< Updated upstream
-import java.util.Optional;
 import puk.groupware.model.user.User_info;
-import java.util.List;
-=======
-import puk.groupware.model.user.User_Info;
 
 
 
-public interface User_info_jpaRepository extends JpaRepository<User_Info, String> {
->>>>>>> Stashed changes
+public interface User_info_jpaRepository extends JpaRepository<User_info, String> {
 
-    User_Info findByUserIdAndUserPw(String userId, String userPw);
+    User_info findByUserIdAndUserPw(String userId, String userPw);
 
-
-@Repository
-public interface User_info_jpaRepository extends JpaRepository<User_info, String>{
-    // id를 통해 사용자를 조회
-    //optional을 null point exception 방지
-    User_info findByUserId(String userId);
-    // User_info findByUserIdAndUserPw(String userId, String userPw);
 }
