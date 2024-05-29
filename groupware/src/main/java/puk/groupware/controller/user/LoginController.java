1d7b58d56e02d5d9d92fecb51ecd83ddaddb70ab
vinca0224 <vinca0224@naver.com>
2024-05-29 13:46:38 +0900

    login

--- groupware/src/main/java/puk/groupware/controller/user/LoginController.java
+++ groupware/src/main/java/puk/groupware/controller/user/LoginController.java
@@ -1,5 +1,6 @@
 package puk.groupware.controller.user;
 
+<<<<<<< Updated upstream
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,4 +13,50 @@ public class LoginController {
         return "loginform";
     }
     
+=======
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import jakarta.servlet.http.HttpSession;
+import puk.groupware.model.user.User_Info;
+import puk.groupware.service.user.UserLoginService;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@Controller
+public class LoginController {
+
+    private final UserLoginService userLoginService;
+    private final HttpSession httpSession;
+    @Autowired
+    LoginController(UserLoginService userLoginService, HttpSession httpSession){
+        this.userLoginService = userLoginService;
+        this.httpSession = httpSession;
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "loginform";
+    }
+    
+
+    @PostMapping("/loginRequest")
+    public String login(@RequestParam(name="userId") String userId, @RequestParam(name="userPw") String userPw, Model model) {
+        User_Info user = userLoginService.findUser(userId,userPw);
+        if(user != null){
+            httpSession.setAttribute("loginedUser", user);
+            return "redirect:/";
+        }else{
+            model.addAttribute("error", "아이디나 비밀번호가 불일치합니다.");
+            return "loginform";
+        }
+
+
+    }
+    
+>>>>>>> Stashed changes
 }
--- groupware/src/main/java/puk/groupware/repository/user/User_info_jpaRepository.java
+++ groupware/src/main/java/puk/groupware/repository/user/User_info_jpaRepository.java
@@ -5,10 +5,19 @@ package puk.groupware.repository.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+<<<<<<< Updated upstream
 import java.util.Optional;
 import puk.groupware.model.user.User_info;
 import java.util.List;
+=======
+import puk.groupware.model.user.User_Info;
 
+
+
+public interface User_info_jpaRepository extends JpaRepository<User_Info, String> {
+>>>>>>> Stashed changes
+
+    User_Info findByUserIdAndUserPw(String userId, String userPw);
 
 
 @Repository
