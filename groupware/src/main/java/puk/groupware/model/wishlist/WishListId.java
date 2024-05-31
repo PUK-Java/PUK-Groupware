a21405719c9b003d6e93e728f66c639a730b093e
KangJeongTaek <dfgddjd@naver.com>
2024-05-31 10:03:40 +0900

    @Date 대신 @Getter @setter 설정

--- groupware/src/main/java/puk/groupware/model/wishlist/WishListId.java
+++ groupware/src/main/java/puk/groupware/model/wishlist/WishListId.java
@@ -6,14 +6,16 @@ import jakarta.persistence.Embeddable;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import puk.groupware.model.project.Project_info;
 import puk.groupware.model.user.User_Info;
 
 @NoArgsConstructor
 @Embeddable
-@Data
+@Getter
+@Setter
 public class WishListId implements Serializable{
 
     @ManyToOne(fetch = FetchType.LAZY)
