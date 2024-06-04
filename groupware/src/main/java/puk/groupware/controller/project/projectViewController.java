be85edbb9f0ad79706e87d897ef3942b64152199
simwh123 <tlatlek@naver.com>
2024-06-04 17:16:17 +0900

    커밋하기

--- groupware/src/main/java/puk/groupware/controller/project/projectViewController.java
+++ groupware/src/main/java/puk/groupware/controller/project/projectViewController.java
@@ -1,6 +1,5 @@
 package puk.groupware.controller.project;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +13,6 @@ import puk.groupware.model.project.Project_info;
 import puk.groupware.model.user.User_Info;
 import puk.groupware.service.project.ProjectSupportService;
 import puk.groupware.service.project.ProjectViewService;
-import puk.groupware.service.wishList.WishListService;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,19 +23,9 @@ public class projectViewController {
     @Autowired
     private ObjectMapper objectMapper;
 
-
-
     private final ProjectViewService viewService;
     private final ProjectSupportService supportService;
     private final HttpSession HttpSession;
-
-    // 프로젝트 정보를 뷰에 띄우기
-    @GetMapping("/projectDetail/{id}")
-    public String prjview(@PathVariable("id") Long id, Model model) {
-        // 프로젝트 객체 찾기
-        Project_info projectView = viewService.getProjectById(id);
-        // 시작날짜와 끝날짜 사이를 계산하기위한 서비스메소드
-        // 가져온 값은 long 타입으로 전환
 
     @Autowired
     projectViewController(HttpSession HttpSession,
@@ -60,7 +48,7 @@ public class projectViewController {
         // 뷰에서 쓰기 위하여 모델이 값을 넣는다.
         model.addAttribute("data", projectView);
         model.addAttribute("daysBetween", daysBetween);
-      
+
         // projectView.getUserId() 같은경우 객체이기때문에 스트링으로 변환하기 위한 작업
         // null값이 있을경우 오류가 발생하기때문에 조건문으로 처리
         if (projectView.getUserId() != null) {
@@ -70,6 +58,7 @@ public class projectViewController {
 
         // 처음 접속할 때 찜목록에 해당 프로젝트가 있는지 없는지 체크하기 위해 추가
         model.addAttribute("wishListCheck", wishListService.checkWishList(projectView));
+
         model.addAttribute("count", supportCount);
         return "projectDetail";
     }
--- groupware/src/main/java/puk/groupware/service/project/ProjectFindService.java
+++ groupware/src/main/java/puk/groupware/service/project/ProjectFindService.java
@@ -19,89 +19,93 @@ public class ProjectFindService {
     private final Project_info_jpaRepository pRepository;
 
     @Autowired
-    ProjectFindService(Project_info_jpaRepository pRepository){
+    ProjectFindService(Project_info_jpaRepository pRepository) {
         this.pRepository = pRepository;
     }
 
-    //조건 없이 모두 찾기
-    public Page<Project_info> findAllPage(Pageable page){
+    // 조건 없이 아이디만으로 찾기
+    public Project_info findById(Long projectNo) {
+        return pRepository.findById(projectNo).orElseThrow();
+    }
+
+    // 조건 없이 페이지 모두 찾기
+    public Page<Project_info> findAllPage(Pageable page) {
         return pRepository.findAll(page);
     }
 
-    //이름을 조건으로 찾기
-    public Page<Project_info> findByTitleLike(String title,Pageable page){
+    // 이름을 조건으로 찾기
+    public Page<Project_info> findByTitleLike(String title, Pageable page) {
         return pRepository.findByTitleContains(title, page);
     }
 
-    //카테고리 별로 찾기
-    public Page<Project_info> findByCategory(String category,Pageable page){
-        return pRepository.findByCategory(category,page);
+    // 카테고리 별로 찾기
+    public Page<Project_info> findByCategory(String category, Pageable page) {
+        return pRepository.findByCategory(category, page);
     }
 
-    //카테고리와 이름 모든 조건으로 찾기
-    public Page<Project_info> findByTitleContainsAndCategory(String title,String category,Pageable page){
-        return pRepository.findByTitleContainsAndCategory(title,category,page);
+    // 카테고리와 이름 모든 조건으로 찾기
+    public Page<Project_info> findByTitleContainsAndCategory(String title, String category, Pageable page) {
+        return pRepository.findByTitleContainsAndCategory(title, category, page);
     }
-
 
     // 모든 컬럼의 Description의 길이를 확인해서 길이를 줄여주는 작업 작업
-    public void descriptionSummary(List<Project_info> projects){
-        for(Project_info project: projects){
+    public void descriptionSummary(List<Project_info> projects) {
+        for (Project_info project : projects) {
             String description = project.getDescription();
-            if(description.length()> 100){
+            if (description.length() > 100) {
                 description = description.substring(0, 100) + "...";
                 project.setDescription(description);
             }
         }
     }
 
-    //Page 객체를 받아서 List으로 변환한 다음 모델에 추가해주는 작업
-    public void pagetoListAddtoModel(Model model,Page<Project_info> pageNumber){
+    // Page 객체를 받아서 List으로 변환한 다음 모델에 추가해주는 작업
+    public void pagetoListAddtoModel(Model model, Page<Project_info> pageNumber) {
         List<Project_info> projects = pageNumber.getContent();
         descriptionSummary(projects);
         model.addAttribute("projectPage", pageNumber.getNumber());
-        model.addAttribute("projectTotalPage",pageNumber.getTotalPages());
+        model.addAttribute("projectTotalPage", pageNumber.getTotalPages());
         model.addAttribute("projects", projects);
 
     }
 
-    //페이지 설정
-    public void paging(int page,String projectName, String projectCategory,Model model){
-        PageRequest pageable = PageRequest.of(page,9,Sort.by("startDate").descending());
+    // 페이지 설정
+    public void paging(int page, String projectName, String projectCategory, Model model) {
+        PageRequest pageable = PageRequest.of(page, 9, Sort.by("startDate").descending());
 
-        //빈 문자열이 들어오면 널로 바꾸시오.
-        if(projectName!= null){
-            if(projectName.equals("")){
+        // 빈 문자열이 들어오면 널로 바꾸시오.
+        if (projectName != null) {
+            if (projectName.equals("")) {
                 projectName = null;
             }
         }
-        //카테고리 역시 빈 문자열로 들어오면 널로 바꾸시오
-        if(projectCategory != null){
-            if(projectCategory.equals("")){
+        // 카테고리 역시 빈 문자열로 들어오면 널로 바꾸시오
+        if (projectCategory != null) {
+            if (projectCategory.equals("")) {
                 projectCategory = null;
             }
         }
 
-        //프로젝트 이름과 카테고리에 조건이 없다면
-        if((projectName == null) && (projectCategory == null)){
+        // 프로젝트 이름과 카테고리에 조건이 없다면
+        if ((projectName == null) && (projectCategory == null)) {
             Page<Project_info> pageNumber = findAllPage(pageable);
             pagetoListAddtoModel(model, pageNumber);
         }
 
-        //프로젝트 이름의 조건만 있다면
-        if((projectName != null) && (projectCategory == null)){
+        // 프로젝트 이름의 조건만 있다면
+        if ((projectName != null) && (projectCategory == null)) {
             Page<Project_info> pageNumber = findByTitleLike(projectName, pageable);
             pagetoListAddtoModel(model, pageNumber);
         }
 
-        //카테고리 조건만 있다면
-        if((projectCategory != null) && (projectName == null)){
+        // 카테고리 조건만 있다면
+        if ((projectCategory != null) && (projectName == null)) {
             Page<Project_info> pageNumber = findByCategory(projectCategory, pageable);
             pagetoListAddtoModel(model, pageNumber);
         }
 
-        //프로젝트 이름과 카테고리 조건이 모두 있다면
-        if((projectName != null) && (projectCategory != null)){
+        // 프로젝트 이름과 카테고리 조건이 모두 있다면
+        if ((projectName != null) && (projectCategory != null)) {
             Page<Project_info> pageNumber = findByTitleContainsAndCategory(projectName, projectCategory, pageable);
             pagetoListAddtoModel(model, pageNumber);
         }
